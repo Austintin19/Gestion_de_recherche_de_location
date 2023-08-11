6597982e5fcc40089338e36c639b683a9fe33b23
Léo <134875150+cyr-leo@users.noreply.github.com>
2023-08-11 09:18:51 +0100

    change

--- Contact/contact_us.php
+++ Contact/contact_us.php
@@ -1,17 +1,41 @@
-<?php include '../base_header.php' ?>
-<link href="contact_us.css" >
-<div id="contact" class="containing">
-    <h1>Contactez-nous</h1>
-    <form action="#" method="post">
-        <label for="nom">Nom :</label>
-        <input type="text" id="nom" name="nom" required>
+<link href="contact_us.css" rel="stylesheet">
+<form action="" method="post">
+    <div>
+        <label for="name">Votre nom</label>
+        <input type="text" name="name" id="name" required />
+    </div>
+    <div>
+        <label for="email">Votre adresse e-mail</label>
+        <input type="email" name="email" id="email" required />
+    </div>
+    <div>
+        <label for="subject">Objet</label>
+        <input type="text" name="subject" id="subject" required />
+    </div>
+    <div>
+        <label for="message">Votre message</label>
+        <textarea name="message" id="message" required></textarea>
+    </div>
+    <div>
+        <input type="submit" value="Envoyer" />
+    </div>
+</form>
+<?php
+// Vérifie si le formulaire a été soumis
+if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['subject']) && isset($_POST['message'])) {
+    // Récupère les données du formulaire
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
 
-        <label for="email">Email :</label>
-        <input type="text" id="email" name="email" required>
+    // Envoie un e-mail au propriétaire du site
+    mail('contact@example.com', $subject, $message, 'From: ' . $email);
 
-        <label for="message">Message :</label>
-        <textarea id="message" name="message" rows="5" required></textarea>
-
-        <input type="submit" value="Envoyer">
-</div>
-<?php include 'base_footer.php' ?>
+    // Affiche un message de confirmation
+    echo '<p>Votre message a bien été envoyé.</p>';
+} else {
+    // Le formulaire n'a pas été soumis
+    echo '<p>Veuillez remplir tous les champs du formulaire.</p>';
+}
+?>
