c0cc0932100765d9550a0a0aea51824bf282f115
Austintin19 <austinvitoessi@gmail.com>
2023-08-11 15:32:45 +0100

    Modification au niveau de certaines pages

--- Configs/config.php
+++ Configs/config.php
@@ -1,12 +1,22 @@
-<?php 
-$servername = "localhost";
-        $dbUsername = "root";
-        $dbPassword = "";
-        $dbname = "test";
+<?php
 
-        $conn = new mysqli($servername, $dbUsername, $dbPassword, $dbname);
-        if ($conn->connect_error) {
-            die("Échec de la connexion : " . $conn->connect_error);
-        }
-        
+// Définir les paramètres de connexion à la base de données
+$dsn = "mysql:host=localhost;dbname=gestion_recherche_location";
+$username = "root";
+$password = "";
+
+// Créer une instance PDO
+$conn = new PDO($dsn, $username, $password);
+
+// Définir le mode d'erreur sur les exceptions
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+// Si la connexion à la base de données échoue, lancer une exception
+if ($conn->connect_error) {
+    throw new Exception("Échec de la connexion : " . $conn->connect_error);
+}
+
+// La connexion a réussi !
+echo "Connexion réussie à la base de données.";
+
 ?>
--- Connexion/connexion.php
+++ Connexion/connexion.php
@@ -1,5 +1,6 @@
 <?php
-  ob_start(); // Démarre la mise en tampon de sortie
+global $prenom;
+ob_start(); // Démarre la mise en tampon de sortie
   session_start(); // Démarre une session
 require('../Configs/config.php')
 ?>
@@ -9,71 +10,98 @@ require('../Configs/config.php')
 <head>
   <title>Page de connexion</title>
   <link rel="stylesheet" href="connexion.css">
+    <link rel="stylesheet" href="bootstrap/bootstrap-3.4.1-dist/css/bootstrap.min.css" crossorigin="anonymous">
+    <script src="bootstrap/bootstrap-3.4.1-dist/js/bootstrap.min.js"></script>
+    <script src="bootstrap/bootstrap-3.4.1-dist/js/jquery-3.7.0.min.js"></script>
+    <script src="https://unpkg.com/vue"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.6.12/vue.min.js"></script>
 </head>
 <body>
-  <?php
-    $error_message = "";
+<?php
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-      $username = $_POST['username'];
-      $password = $_POST['password'];
-      $prenom = $_POST['prenom'];
+// Définir les paramètres de connexion à la base de données
+$dsn = "mysql:host=localhost;dbname=gestion_recherche_location";
+$username = "root";
+$password = "";
 
-      if (strlen($username) < 4) {
-        $error_message = "Le nom d'utilisateur doit faire au moins 4 caractères.";
-      } elseif (strlen($password) < 6 && $username != 'admin') {
-        $error_message = "Le mot de passe doit faire au moins 6 caractères.";
-      }  else {
-        
+// Créer une instance PDO
+$conn = new PDO($dsn, $username, $password);
 
-        // Échapper les caractères spéciaux pour éviter les injections SQL
-        $escaped_username = $conn->real_escape_string($username);
-        $escaped_password = $conn->real_escape_string($password);
-        $escaped_prenom = $conn->real_escape_string($prenom);
+// Définir le mode d'erreur sur les exceptions
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        // Vérifier les identifiants dans la base de données
-        $sql = "SELECT * FROM users WHERE nom = '$escaped_username' AND mot_de_passe = '$escaped_password' AND prenom = '$escaped_prenom'";
-        $result = $conn->query($sql);
-        
+// Échapper les caractères spéciaux pour éviter les injections SQL
+$escaped_username = $conn->htmlspecialchars($username);
+$escaped_password = $conn->htmlspecialchars($password);
+$escaped_prenom = $conn->htmlspecialchars($prenom);
 
-        if ($result->num_rows > 0) {
-          // Identifiants valides, enregistre les informations dans la session
-          $_SESSION['username'] = $username;
-          $_SESSION['password'] = $password;
-          $_SESSION['username'] = $username;
+// Vérifier les identifiants dans la base de données
+$sql = "SELECT * FROM users WHERE nom = :username AND mot_de_passe = :password AND prenom = :prenom";
+$stmt = $conn->prepare($sql);
+$stmt->bindParam(':username', $escaped_username);
+$stmt->bindParam(':password', $escaped_password);
+$stmt->bindParam(':prenom', $escaped_prenom);
+$stmt->execute();
 
-          // Rediriger vers la page d'accueil
-          header("Location: ../");
-          exit();
-        } else {
-          // Identifiants incorrects
-          $error_message = "Identifiants incorrects.";
-        }
+// Si la requête a réussi, vérifier si l'utilisateur existe
+if ($stmt->rowCount() > 0) {
+    // L'utilisateur existe, enregistrer les informations dans la session
+    $_SESSION['username'] = $username;
+    $_SESSION['password'] = $password;
+    $_SESSION['prenom'] = $prenom;
 
-        $conn->close();
-      }
-    }
-  ?>
-<div class="all">
-  <div class="connexio">
+    // Rediriger vers la page d'accueil
+    header("Location: ../");
+    exit();
+} else {
+    // L'utilisateur n'existe pas, afficher un message d'erreur
+    $error_message = "Identifiants incorrects.";
+}
 
-    <h2 class="p" >Connexion</h2>
-    <form id="login-form" method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-    <div class="error-message"><?php echo $error_message; ?></div>
-    <label for="email">Nom</label>
-        <input class="email" type="text" name="username" placeholder="Nom">
-    <label for="prenom">Prénom</label>
-        <input class="email" type="text" name="prenom" placeholder = "Prénom d'utilisateur">
-    <label for="pass">Mot de passe</label>
-        <input class="email" type="password" id="pass" name="password" placeholder="Mot de passe" placeholder="Entrez votre mot de passe">
+?>
 
-        <div class="option">
-            <a class="ins" href="Inscription.php">Vous n'aves pas encore de compte? S'inscrire</a>
-        </div>
-        <input class="connexion" type="submit" value="Connexion &#8594" />
-    </form>
-    
-  </div>
+<!DOCTYPE html>
+  <html>
+  <head>
+      <title>Page de connexion</title>
+      <link rel="stylesheet" href="bootstrap/bootstrap-3.4.1-dist/css/bootstrap.min.css" crossorigin="anonymous">
+      <link rel="stylesheet" href="connexion.css" >
+      <script src="bootstrap/bootstrap-3.4.1-dist/js/bootstrap.min.js"></script>
+      <script src="bootstrap/bootstrap-3.4.1-dist/js/jquery-3.7.0.min.js"></script>
+      <script src="https://unpkg.com/vue"></script>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.6.12/vue.min.js"></script>
+  </head>
+  <body>
+  <div class="container-sm ">
+      <div class="row justify-content-center">
+          <div class="col-md-4">
+              <h2 class="text-center text-light">Connexion</h2>
+              <form action="connexion.php" method="POST">
+                  <div class="form-group mb-3">
+                      <label for="name" class="form-label text-light">Nom</label>
+                      <input type="text" class="form-control" id="name" name="username" placeholder="Nom d'utilisateur">
+                  </div>
+                  <div class="form-group mb-3">
+                      <label for="prenom" class="form-label text-light">Prénom</label>
+                      <input type="text" class="form-control" id="prenom" name="prenom" placeholder="Prénom d'utilisateur">
+                  </div>
+                  <div class="form-group mb-3">
+                      <label for="pass" class="form-label text-light">Mot de passe</label>
+                      <input type="password" class="form-control" id="pass" name="password" placeholder="Mot de passe">
+                  </div>
+                  <div class="form-group mb-3 mt-3">
+                      <a href="Inscription.php" class="btn btn-secondary">Vous n'avez pas encore de compte? S'inscrire</a>
+                  </div>
+                  <div class="form-group mb-3 text-center">
+                    <button type="submit" class="btn btn-primary ">Connexion &rarr;</button>
+                  </div>
+              </form>
+          </div>
+      </div>
   </div>
   <?php
     ob_end_flush(); // Envoie le tampon de sortie
